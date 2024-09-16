321f630a12b0130a392cd64d1a316ca91f5ada07
StanislavSova <stanislav.sova@dvigunity.com>
2024-09-16 23:43:03 +0300

    РW: 16/9/2024

--- 05_/fs_async.js
+++ 05_/fs_async.js
@@ -1,46 +1,36 @@
+import fs from 'node:fs';
 import path from 'node:path';
-import fs from 'node:fs/promises';
-import { Buffer } from 'node:buffer';
+import { Transform } from 'stream';
 
-const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
 
-const folderPath = path.join(__dirname, 'files');
-const filePath = path.join(folderPath, 'data.txt');
+const file1 = path.join(__dirname, 'files', 'data.txt');
+const file2 = path.join(__dirname, 'files', 'data2.txt');
 
-const buff = Buffer.from('NODEJS Programm');
+const readStream = fs.createReadStream(file1, {
+    encoding: 'utf8',
+});
 
-async function createFolderIfNotExists(folderPath) {
-    try {
-        await fs.mkdir(folderPath, { recursive: true });
-        console.log('Папка успешно создана или уже существует');
-    } catch (err) {
-        console.error('Ошибка при создании папки:', err);
+const writeStream = fs.createWriteStream(file2, {
+    encoding: 'utf8',
+});
+
+const upperCaseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, chunk.toString().toUpperCase());
+    },
+});
+
+readStream.on('data', (chunk) => {
+    let i = 0;
+    function printChar() {
+        if (i < chunk.length) {
+            process.stdout.write(chunk[i]);
+            i++;
+            setTimeout(printChar, 100);
+        }
     }
-}
+    printChar();
+});
 
-async function writeBufferToFile(filePath, buffer) {
-    try {
-        await fs.writeFile(filePath, buffer);
-        console.log('Файл успешно записан');
-    } catch (err) {
-        console.error('Ошибка при записи файла:', err);
-    }
-}
-
-async function readFileContent(filePath) {
-    try {
-        const data = await fs.readFile(filePath);
-        console.log('Содержимое файла:', data.toString());
-    } catch (err) {
-        console.error('Ошибка при чтении файла:', err);
-    }
-}
-
-async function run() {
-    await createFolderIfNotExists(folderPath);
-    await writeBufferToFile(filePath, buff);
-    await readFileContent(filePath);
-}
-
-run();
+readStream.pipe(upperCaseTransform).pipe(writeStream);
